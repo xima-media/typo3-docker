d51f374e1e2a14b23f5ab019c54f11724987e50e
martipoe <130643472+martipoe@users.noreply.github.com>
2025-02-11 16:45:35 +0100

    feat: add Dockerfile and Github Actions build workflow, resolve relative paths

--- sitepackage-req-checker/sitepackage-req-checker.sh
+++ sitepackage-req-checker/sitepackage-req-checker.sh
@@ -2,15 +2,29 @@
 
 # usage: /bin/bash ./check_typo3_requirements.sh "packages/xima_sitepackage/composer.json"
 
-# define path of sitepackage composer.json
+# ensure root composer-json is present
+ROOT_COMPOSER_FILE="$PWD/composer.json"
+if [ ! -f "${ROOT_COMPOSER_FILE}" ]; then
+    printf "\\e[0;31mError: Root composer.json is not present in current directory.\n\\e[0m"
+    exit 1
+fi
+
+# ensure vendor directory is present
+VENDOR_DIR="$PWD/vendor"
+if [ ! -d "${VENDOR_DIR}" ]; then
+    printf "\\e[0;31mError: Vendor directory not present in current directory.\n\\e[0m"
+    exit 1
+fi
+
+# Validate path of sitepackage composer.json
 if [[ -n "$1" ]]; then
-    SITEPACKAGE_COMPOSER_JSON="$1"
+    SITEPACKAGE_COMPOSER_JSON="$PWD/$1"
     if [ ! -f ${SITEPACKAGE_COMPOSER_JSON} ]; then
-        printf "\\e[0;31mError: '${SITEPACKAGE_COMPOSER_JSON}' does not exist.\n\\e[0m"
+        printf "\\e[0;31mError: '${SITEPACKAGE_COMPOSER_JSON}' not found within current directory.\n\\e[0m"
         exit 1
     fi
 else
-    printf "\\e[0;31mError: Provide path to composer.json of sitepackage as first argument. Usage: \'./check_typo3_requirements.sh "packages/xima_sitepackage/composer.json\'"\n\\e[0m"
+    printf "\\e[0;31mError: Provide relative path to composer.json of sitepackage as first argument. Usage: \'./check_typo3_requirements.sh "packages/xima_sitepackage/composer.json\'"\n\\e[0m"
     exit 1
 fi
 
@@ -18,7 +32,6 @@ fi
 set -euo pipefail
 
 PACKAGES_MISSING=()
-SUBDEPENDENCIES_ALL=()
 
 # ensure composer.json of sitepackage has require object
 if [[ "$(jq -r 'has("require")' "${SITEPACKAGE_COMPOSER_JSON}")" != "true" ]]; then
@@ -30,14 +43,14 @@ fi
 SITEPACKAGE_NAME=$(jq -r '.name' "${SITEPACKAGE_COMPOSER_JSON}")
 
 # retrieve all dependencies from root composer.json excluding the sitepackage itself
-REQUIRES=$(jq -r --arg SITEPACKAGE_NAME "${SITEPACKAGE_NAME}"  'del(.require[$SITEPACKAGE_NAME]) | .require | keys[]' composer.json)
+REQUIRES=$(jq -r --arg SITEPACKAGE_NAME "${SITEPACKAGE_NAME}"  'del(.require[$SITEPACKAGE_NAME]) | .require | keys[]' "${ROOT_COMPOSER_FILE}")
 
 # loop through all dependencies
 for REQUIRE in ${REQUIRES[@]}; do
     # cat their composer.json
-    if [ -f "vendor/${REQUIRE}/composer.json" ]; then
+    if [ -f "${VENDOR_DIR}/${REQUIRE}/composer.json" ]; then
         # extract composer type
-        PACKAGE_TYPE=$(jq -r '.type' "vendor/${REQUIRE}/composer.json")
+        PACKAGE_TYPE=$(jq -r '.type' "${VENDOR_DIR}/${REQUIRE}/composer.json")
         # and check if == "typo3-cms-extension"
         if [[ "${PACKAGE_TYPE}" == "typo3-cms-extension" ]]; then
             # if so, check if the package is requirement in sitepackage
